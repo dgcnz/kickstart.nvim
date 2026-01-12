2870523678ccaa9546250c8a1d686a907a2fa147
Diego Canez <canezdiego@gmail.com>
2026-01-12 13:57:38 +0100

    feat: obsidian.nvim

--- init.lua
+++ init.lua
@@ -822,6 +822,37 @@ require('lazy').setup({
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
+  {
+    'epwalsh/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+      -- refer to `:h file-pattern` for more examples
+      'BufReadPre /Users/dgcnz/Documents/Obsidian Vault/*.md',
+      'BufNewFile /Users/dgcnz/Documents/Obsidian Vault/*.md',
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'hrsh7th/nvim-cmp',
+      'nvim-telescope/telescope.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'personal',
+          path = '~/Documents/Obsidian Vault/',
+        },
+      },
+      ui = {
+        enable = false,
+      },
+      disable_frontmatter = true,
+    },
+  },
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
